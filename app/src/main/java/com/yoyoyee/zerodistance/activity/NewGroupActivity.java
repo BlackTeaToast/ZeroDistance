c22f2d8f664bbe53d77c7ead313c299777b56ebc
Chien-Hong Liao <p12355668@gmail.com>
2016-03-15 23:26:23 +0800

    Add new activities

---
+++ app/src/main/java/com/yoyoyee/zerodistance/activity/NewGroupActivity.java
@@ -0,0 +1,15 @@
+package com.yoyoyee.zerodistance.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yoyoyee.zerodistance.R;
+
+public class NewGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_group);
+    }
+}
