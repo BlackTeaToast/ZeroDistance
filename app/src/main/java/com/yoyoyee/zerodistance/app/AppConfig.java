7bc98fdeabdddfbe79e28b98e66b364325ad40b2
Chien-Hong Liao <p12355668@gmail.com>
2016-03-15 21:54:57 +0800

    Initial commit

---
+++ app/src/main/java/com/yoyoyee/zerodistance/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.yoyoyee.zerodistance.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://blackteatoast.asuscomm.com:3000/android_login_api/login";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://blackteatoast.asuscomm.com:3000/android_login_api/register";
+}
