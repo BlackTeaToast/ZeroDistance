20150c36c99ace730e83a3ff1c7f092bf7e3061f
PatrickC <pat31718@yahoo.com.tw>
2016-03-30 02:03:42 +0800

    在app新增了
    QAndA
    Questions
    UsedData
    
    的Class 主要是new UsedData出來使用

---
+++ app/src/main/java/com/yoyoyee/zerodistance/app/QAndA.java
@@ -0,0 +1,45 @@
+package com.yoyoyee.zerodistance.app;
+
+/**
+ Created by PatrickC on 2016/3/29.
+ */
+public class QAndA {
+    String	questionUser;//	存放問問題的人的暱稱
+    QAndA	next;//	指到下一個詢問者的問題串
+    Questions	firstQuestion;//	存放當前詢問者的第一個問題
+    public  QAndA(){
+        this.firstQuestion = null;
+        this.next = null;
+        this.questionUser = null;
+    }
+    public  QAndA(String QuestionUser, String content){
+        this.firstQuestion = new Questions(true, content);
+        this.next = null;
+        this.questionUser = QuestionUser;
+    }
+
+    public void nextQAndA(String QuestionUser, String content){
+        QAndA newQAndA = new QAndA();
+        newQAndA.firstQuestion =  new Questions(true, content);
+        newQAndA.questionUser = questionUser;
+        newQAndA.next = null;
+        this.next = newQAndA;
+
+    }
+    //回傳最後一節 QAndA
+    public QAndA findFinal(){
+        QAndA index;
+        index = this;
+        while(index.next!=null){
+            index = index.next;
+        }
+        return index;
+    }
+    //到最後一節QAndA 新增一個新的 QandA 並加上第一個Question
+    public void newQAndA(String QuestionUser, String content){
+        QAndA index;
+        index = this.findFinal();
+        index.nextQAndA(QuestionUser, content);
+    }
+
+}
---
+++ app/src/main/java/com/yoyoyee/zerodistance/app/Questions.java
@@ -0,0 +1,44 @@
+package com.yoyoyee.zerodistance.app;
+
+/**
+  Created by PatrickC on 2016/3/29.
+ */
+public class Questions {
+    boolean	isQuestion;//	question = true; answer = false
+    String	content;//	內容
+    Questions	next;//	指到下一個question or answer
+    public  Questions(){
+        this.isQuestion = true;
+        this.content =null;
+        this.next = null;
+    }
+    public Questions(boolean isQuestion, String content){
+        this.isQuestion = true;
+        this.content =null;
+        this.next = null;
+    }
+
+    public void nextQuestions(boolean isQuestion, String content){
+       Questions newQuestion = new Questions();
+       newQuestion.isQuestion = isQuestion;
+       newQuestion.content = content;
+       this.next = newQuestion;
+     }
+
+    //回傳此 QandA 的最後一節 Questions
+    public Questions findFinal(){
+        Questions index;
+        index = this;
+        while(index.next!=null){
+            index = index.next;
+        }
+        return index;
+    }
+
+    //到最後一節Quetions 新增一個新的 Question
+    public void newQuestions(boolean isQuestion, String content){
+        Questions index;
+        index = this.findFinal();
+        index.nextQuestions(isQuestion , content);
+    }
+}
