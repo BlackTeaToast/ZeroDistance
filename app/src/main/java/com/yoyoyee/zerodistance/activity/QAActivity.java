d0f896a94c7ce63a073b0e26215f887c995acfb6
Falan <candy104jenny@gmail.com>
2016-03-31 00:19:04 +0800

    Merge branch 'master' of F:\Hong\YoYoYeeee\ZeroDistance with conflicts.

--- app/src/main/java/com/yoyoyee/zerodistance/activity/QAActivity.java
+++ app/src/main/java/com/yoyoyee/zerodistance/activity/QAActivity.java
@@ -1,12 +1,9 @@
 package com.yoyoyee.zerodistance.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.Layout;
-import android.widget.Button;
-import android.widget.RelativeLayout;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
-import com.yoyoyee.zerodistance.R;
+        import com.yoyoyee.zerodistance.R;
 
 public class QAActivity extends AppCompatActivity {
 
@@ -14,6 +11,5 @@ public class QAActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qa);
-
     }
 }
