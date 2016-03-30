6b5a3cff32d4652d7ead2cc36a6bfd2a71e9d26e
Falan <candy104jenny@gmail.com>
2016-03-31 01:31:24 +0800

    Falan

--- app/src/main/java/com/yoyoyee/zerodistance/activity/QAActivity.java
+++ app/src/main/java/com/yoyoyee/zerodistance/activity/QAActivity.java
@@ -1,15 +1,26 @@
 package com.yoyoyee.zerodistance.activity;
 
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
         import com.yoyoyee.zerodistance.R;
 
 public class QAActivity extends AppCompatActivity {
-
+    Button GO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qa);
+        GO = (Button)findViewById(R.id.for_Q_Button);
+        GO.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QAActivity.this, AskActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
