371b7a34ca54e4487ba45040c72074ce7b3b4f30
PatrickC <pat31718@yahoo.com.tw>
2016-04-01 00:05:11 +0800

    修改UsedData (整合變數) ; 新增WhoJoin.java 進app

---
+++ app/src/main/java/com/yoyoyee/zerodistance/app/WhoJoin.java
@@ -0,0 +1,14 @@
+package com.yoyoyee.zerodistance.app;
+
+/**
+  Created by PatrickC on 2016/3/31.
+ */
+public class WhoJoin {
+    public String userID;
+    public WhoJoin next;
+
+    public WhoJoin(){
+        userID = null;
+        next = null;
+    }
+}
