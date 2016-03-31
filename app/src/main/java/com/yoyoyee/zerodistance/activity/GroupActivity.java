402114864cb7d690baf97d17329deab15737967d
PatrickC <pat31718@yahoo.com.tw>
2016-04-01 04:11:52 +0800

    主要是新增 group misson的xml

--- app/src/main/java/com/yoyoyee/zerodistance/activity/GroupActivity.java
+++ app/src/main/java/com/yoyoyee/zerodistance/activity/GroupActivity.java
@@ -1,15 +1,43 @@
 package com.yoyoyee.zerodistance.activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.yoyoyee.zerodistance.R;
 
 public class GroupActivity extends AppCompatActivity {
 
+    //變數區=====================================
+    private Toolbar toolbar;
+    private boolean isMission;
+
+    //============================================
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
+        //findViewById--------------------------------------------------------------
+        toolbar = (Toolbar)findViewById(R.id.group_tool_bar);
+
+
+
+        //-------------------------------------------------------------------------------
+
+
+        // 需先讀進以下變數才能正常顯示============================
+        isMission = false;
+
+        // =========================================================
+
+        //設置toolbar標題
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(isMission){actionBar.setTitle("任務");}
+        else{actionBar.setTitle("揪團");}
+
+
     }
 }
