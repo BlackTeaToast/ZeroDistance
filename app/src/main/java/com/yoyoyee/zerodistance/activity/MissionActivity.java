8068df04247fed1b3fd1282d98e508a6338840d1
PatrickC <pat31718@yahoo.com.tw>
2016-04-04 23:32:44 +0800

    Merge branch 'master' of C:\Users\PatrickC\OneDrive\Android Studio\ZeroDistance with conflicts.

--- app/src/main/java/com/yoyoyee/zerodistance/activity/MissionActivity.java
+++ app/src/main/java/com/yoyoyee/zerodistance/activity/MissionActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.yoyoyee.zerodistance.R;
 
@@ -12,7 +14,7 @@ public class MissionActivity extends AppCompatActivity {
     //變數區=====================================
     private Toolbar toolbar;
     private boolean isMission;
-
+    private CheckBox checkBox;
     //============================================
 
     @Override
@@ -21,6 +23,7 @@ public class MissionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mission);
         //findViewById--------------------------------------------------------------
         toolbar = (Toolbar)findViewById(R.id.mission_tool_bar);
+        checkBox = (CheckBox)findViewById(R.id.checkBox2);
         
 
         //-------------------------------------------------------------------------------
@@ -36,6 +39,14 @@ public class MissionActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if(isMission){actionBar.setTitle("任務");}
         else{actionBar.setTitle("揪團");}
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+
 
 
     }
