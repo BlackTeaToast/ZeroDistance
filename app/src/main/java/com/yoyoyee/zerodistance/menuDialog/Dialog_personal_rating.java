419ee06d511bbef380e605b4f48bce7c01abad70
PatrickC <pat31718@yahoo.com.tw>
2016-09-01 03:50:23 +0800

    現在能夠個人評分 一起評分 但是除了database 之外看不出來哪裡有評分。

--- app/src/main/java/com/yoyoyee/zerodistance/menuDialog/Dialog_personal_rating.java
+++ app/src/main/java/com/yoyoyee/zerodistance/menuDialog/Dialog_personal_rating.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 import com.yoyoyee.zerodistance.R;
@@ -28,8 +29,18 @@ public class Dialog_personal_rating extends Dialog{
     private int acceptNumber;//有多少人參加
     private Button finishButton;
     private Context context;
+    private float[] stars;//存放星星評價用
     private ArrayList <MissionAccept> acceptUser;
-    private float[] star = new float[acceptNumber];
+    OnRatingResult mRratingResult;
+
+    //回傳activity用的接口
+    public interface OnRatingResult{
+        void finish(float[] manyStars);
+    }
+
+    public void setRatingResult(OnRatingResult ratingResult){
+        mRratingResult = ratingResult;
+    }
 
     public Dialog_personal_rating(Context context,int acceptNumber) {
         super(context);
@@ -37,6 +48,8 @@ public class Dialog_personal_rating extends Dialog{
         setContentView(R.layout.activity_personal_rating);
         this.acceptNumber = acceptNumber;
         this.context = context;
+        stars = new float[acceptNumber];
+
     }
 
     @Override
@@ -46,14 +59,14 @@ public class Dialog_personal_rating extends Dialog{
 
       // 使用fragment_rate_personal.xml 當作每個 holder 來使用==========
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.personalRecyclerView);
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.personalRecyclerView);
 
         LinearLayoutManager layoutManager;
         layoutManager = new LinearLayoutManager(context);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
 
-        PersonalRateAdapter personalRateAdapter = new PersonalRateAdapter(acceptNumber);
+        final PersonalRateAdapter personalRateAdapter = new PersonalRateAdapter(acceptNumber);
         recyclerView.setAdapter(personalRateAdapter);
 
         //================================================================================
@@ -62,9 +75,15 @@ public class Dialog_personal_rating extends Dialog{
        finishButton.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               Toast.makeText(context, "請按下完成鈕完成最後步驟" ,Toast.LENGTH_SHORT).show();
-               cancel();
 
+               Toast.makeText(context, "請按下完成鈕完成最後步驟" ,Toast.LENGTH_SHORT).show();
+               stars = personalRateAdapter.getStars();
+               //回傳接口
+               if(mRratingResult != null){
+                   mRratingResult.finish(stars);
+               }
+
+               Dialog_personal_rating.this.dismiss();
            }
        });
 
